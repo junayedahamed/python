8a079413c184731fe042d39cb9939152e7fab988
junayedahamed <junayedahamed660@mail.com>
2024-01-31 14:23:22 +0600

    turtle

---
+++ jt.py
@@ -0,0 +1,14 @@
+import turtle
+j=turtle.Turtles()
+# turtle.circle(50)
+# turtle.forward(100)
+for i in range(5):
+    turtle.forward(200)
+    turtle.left(72)
+
+
+
+
+
+turtle.done()
+# j.foward(100)
